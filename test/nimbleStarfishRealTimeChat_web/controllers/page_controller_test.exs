504c3fcd4395d0fc8977c4402cf4b86122662d97
Constantin Cazacu <71124836+constantin-cazacu@users.noreply.github.com>
2023-10-23 15:55:29 +0300

    Update page_controller_test.exs

--- test/nimbleStarfishRealTimeChat_web/controllers/page_controller_test.exs
+++ test/nimbleStarfishRealTimeChat_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule NimbleStarfishRealTimeChatWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Phoenix Chat Example"
   end
 end
