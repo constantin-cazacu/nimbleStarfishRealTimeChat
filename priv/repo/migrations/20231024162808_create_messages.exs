d919b8cf54b6afd3be19f862fa4ff84261d53edc
Constantin Cazacu <71124836+constantin-cazacu@users.noreply.github.com>
2023-10-24 19:45:27 +0300

    Create 20231024162808_create_messages.exs

---
+++ priv/repo/migrations/20231024162808_create_messages.exs
@@ -0,0 +1,12 @@
+defmodule NimbleStarfishRealTimeChat.Repo.Migrations.CreateMessages do
+  use Ecto.Migration
+
+  def change do
+    create table(:messages) do
+      add :name, :string
+      add :message, :string
+
+      timestamps()
+    end
+  end
+end
