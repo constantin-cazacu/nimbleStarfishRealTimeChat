e56f62479d180388b350a3c09acf187a92f8ff17
Constantin Cazacu <71124836+constantin-cazacu@users.noreply.github.com>
2023-10-30 18:42:28 +0200

    added handler for when someone joins chat
    sending existing messages

--- lib/nimbleStarfishRealTimeChat_web/channels/room_channel.ex
+++ lib/nimbleStarfishRealTimeChat_web/channels/room_channel.ex
@@ -4,6 +4,7 @@ defmodule NimbleStarfishRealTimeChatWeb.RoomChannel do
   @impl true
   def join("room:lobby", payload, socket) do
     if authorized?(payload) do
+      send(self(), :after_join)
       {:ok, socket}
     else
       {:error, %{reason: "unauthorized"}}
@@ -25,6 +26,19 @@ defmodule NimbleStarfishRealTimeChatWeb.RoomChannel do
     |> NimbleStarfishRealTimeChat.Repo.insert
     broadcast(socket, "shout", payload)
     {:noreply, socket}
+  end
+
+  # Send existing messages to the client when they join
+  @impl true
+  def handle_info(:after_join, socket) do
+    NimbleStarfishRealTimeChat.Message.get_messages()
+    |> Enum.reverse() # revers to display the latest message at the bottom of the page
+    |> Enum.each(fn msg -> push(socket, "shout", %{
+        name: msg.name,
+        message: msg.message,
+        inserted_at: msg.inserted_at,
+    }) end)
+    {:noreply, socket} # :noreply
   end
 
   # Add authorization logic here as required.
