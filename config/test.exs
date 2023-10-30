698b76a3eb052039614ff8ca838e199c0d459aee
Constantin Cazacu <71124836+constantin-cazacu@users.noreply.github.com>
2023-10-30 19:32:14 +0200

    Update test.exs

--- config/test.exs
+++ config/test.exs
@@ -7,7 +7,7 @@ import Config
 # Run `mix help test` for more information.
 config :nimbleStarfishRealTimeChat, NimbleStarfishRealTimeChat.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "0707",
   hostname: "localhost",
   database: "nimblestarfishrealtimechat_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
