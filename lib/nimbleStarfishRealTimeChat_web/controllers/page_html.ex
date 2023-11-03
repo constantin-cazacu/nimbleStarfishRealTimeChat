b0381060c45f8d156610e3f012bfac5ad8eb7b57
Constantin Cazacu <71124836+constantin-cazacu@users.noreply.github.com>
2023-11-03 09:48:54 +0200

    Update page_html.ex

--- lib/nimbleStarfishRealTimeChat_web/controllers/page_html.ex
+++ lib/nimbleStarfishRealTimeChat_web/controllers/page_html.ex
@@ -2,4 +2,8 @@ defmodule NimbleStarfishRealTimeChatWeb.PageHTML do
   use NimbleStarfishRealTimeChatWeb, :html
 
   embed_templates "page_html/*"
+
+  def person_name(person) do
+    person.givenName || person.name || "guest"
+  end
 end
