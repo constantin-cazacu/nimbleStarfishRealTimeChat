280c14f0018aa4c6217bf9ace888e820366f9a11
Constantin Cazacu <71124836+constantin-cazacu@users.noreply.github.com>
2023-11-03 09:49:03 +0200

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -54,6 +54,7 @@ defmodule NimbleStarfishRealTimeChat.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
+      {:auth_plug, "~> 1.5"},
       {:excoveralls, "~> 0.15.2", only: [:test, :dev]} # tracking test coverage
     ]
   end
